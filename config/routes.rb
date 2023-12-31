2f30438ac0ce1a8243c0ad734db51a160f419865
gudemaru <gudemaru@gudemarunoMacBook-Air.local>
2023-12-31 16:53:27 +0900

    成功ページ、失敗ページの追加

--- config/routes.rb
+++ config/routes.rb
@@ -4,6 +4,8 @@ Rails.application.routes.draw do
   get 'recipes', to: 'recipes#index'
   get 'recipes/show'
   get 'timers', to: 'timers#show'
+  get 'success', to: 'success#index'
+  get 'failure', to: 'failure#index'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
